3e6ca715af4be81bee812edafe0606fef16e56be
Matt Carter <matt.carter@fleeceperformance.com>
2023-03-03 10:31:09 -0500

    update

--- FileCabinet/SuiteScripts/FPE_Scripts/Auto_InvFeed_Ftp/FPE_Sch_AutoInvFeed_FTP.js
+++ FileCabinet/SuiteScripts/FPE_Scripts/Auto_InvFeed_Ftp/FPE_Sch_AutoInvFeed_FTP.js
@@ -7,28 +7,31 @@
  * @NApiVersion 2.1
  * @NScriptType ScheduledScript
  */
-define(["require", "exports", "N/query", "N/sftp", "N/log", "N/file", "N/search", "N/runtime", "N/format"], function (require, exports, query, sftp, log, file, search, runtime, format) {
+define(["require", "exports", "N/query", "N/sftp", "N/log", "N/file", "N/search", "N/runtime", "N/format", "N/email"], function (require, exports, query, sftp, log, file, search, runtime, format, email) {
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.execute = void 0;
     const execute = (scriptContext) => {
-        const sftpConfigIDs = runtime.getCurrentScript().getParameter({ name: 'custscript_fpe_sftpconfid_ids' }).split(',');
-        const csvFolder = runtime.getCurrentScript().getParameter({ name: 'custscript_fpe_csv_folder' });
-        if (sftpConfigIDs.length > 0 && csvFolder) {
-            try {
-                const fileTimestamp = format.format({ value: new Date(), type: format.Type.DATETIME }).replaceAll('/', '').replaceAll(' ', '_').replaceAll(':', '');
-                let csv = 'sku,qty\n';
-                const data = query.runSuiteQL({ query: queryString }).asMappedResults();
-                log.audit('Auto FTP', 'Stock query completed');
-                data.forEach(row => csv += `${row.itemid},${row.quantity}\n`);
-                log.audit('Auto FTP', 'CSV data generated');
-                const csvFile = file.create({
-                    fileType: file.Type.CSV,
-                    name: `FpeStockFeed_${fileTimestamp}.csv`,
-                    folder: csvFolder,
-                    contents: csv
-                });
-                const csvFileID = csvFile.save();
-                log.audit('Auto FTP', 'CSV file created and saved');
+        const currentScript = runtime.getCurrentScript();
+        try {
+            const sftpConfigIDs = currentScript.getParameter({ name: 'custscript_fpe_sftpconfid_ids' }).split(',');
+            const customerIDs = currentScript.getParameter({ name: 'custscript_fpe_emailcustomers_csv' }).split(',');
+            const emailAuthorID = parseInt(currentScript.getParameter({ name: 'custscript_fpe_emailftp_author' }));
+            const csvFolder = currentScript.getParameter({ name: 'custscript_fpe_csv_folder' });
+            const fileTimestamp = format.format({ value: new Date(), type: format.Type.DATETIME }).replaceAll('/', '').replaceAll(' ', '_').replaceAll(':', '');
+            let csv = 'sku,qty\n';
+            const data = query.runSuiteQL({ query: queryString }).asMappedResults();
+            log.audit('Auto FTP', 'Stock query completed');
+            data.forEach(row => csv += `${row.itemid},${row.quantity}\n`);
+            log.audit('Auto FTP', 'CSV data generated');
+            const csvFile = file.create({
+                fileType: file.Type.CSV,
+                name: `FpeStockFeed_${fileTimestamp}.csv`,
+                folder: csvFolder,
+                contents: csv
+            });
+            const csvFileID = csvFile.save();
+            log.audit('Auto FTP', `CSV file created and saved: ${csvFileID}`);
+            if (sftpConfigIDs.length > 0 && csvFolder) {
                 sftpConfigIDs.forEach(config => {
                     let configData = search.lookupFields({
                         type: 'customrecord_fpe_sftp_config',
@@ -36,27 +39,33 @@ define(["require", "exports", "N/query", "N/sftp", "N/log", "N/file", "N/search"
                         columns: ['name', 'custrecord_fpe_sftp_host', 'custrecord_fpe_sftp_hostport', 'custrecord_fpe_sftp_user', 'custrecord_fpe_sftp_passguid', 'custrecord_fpe_sftp_hostkey', 'custrecord_fpe_sftp_directory']
                     });
                     log.audit('Auto FTP', 'sFTP Connection started');
-                    try {
-                        let transfer = sftp.createConnection({
-                            url: configData.custrecord_fpe_sftp_host,
-                            port: parseInt(configData.custrecord_fpe_sftp_hostport),
-                            username: configData.custrecord_fpe_sftp_user,
-                            passwordGuid: configData.custrecord_fpe_sftp_passguid,
-                            hostKey: configData.custrecord_fpe_sftp_hostkey,
-                            directory: configData.custrecord_fpe_sftp_directory
-                        });
-                        log.audit('Auto FTP', 'sFTP Connected');
-                        transfer.upload({ file: csvFile });
-                        log.audit('Auto FTP', 'File Succesfully transferred');
-                    }
-                    catch (connectError) {
-                        log.error('Auto FTP Connection', `There was an error with the FTP connection --- ${connectError}`);
-                    }
+                    let transfer = sftp.createConnection({
+                        url: configData.custrecord_fpe_sftp_host,
+                        port: parseInt(configData.custrecord_fpe_sftp_hostport),
+                        username: configData.custrecord_fpe_sftp_user,
+                        passwordGuid: configData.custrecord_fpe_sftp_passguid,
+                        hostKey: configData.custrecord_fpe_sftp_hostkey,
+                        directory: configData.custrecord_fpe_sftp_directory
+                    });
+                    log.audit('Auto FTP', 'sFTP Connected');
+                    transfer.upload({ file: csvFile });
+                    log.audit('Auto FTP', 'File Successfully transferred');
                 });
             }
-            catch (error) {
-                log.error('Auto FTP', `There was an error with the Auto FTP --- ${error}`);
+            if (customerIDs.length > 0 && csvFolder && emailAuthorID) {
+                log.audit('Auto FTP', 'Email Customers Found, Starting Email Process');
+                email.send({
+                    author: emailAuthorID,
+                    recipients: customerIDs,
+                    subject: 'Fleece Performance Engineering Daily Stock Feed',
+                    body: 'This daily stock feed is representative of our inventory available for drop-shipments only at the time of publishing and does not apply to pending or new stocking orders. Dropship orders for in-stock items received before 2:00 PM EST will be shipped on the same day.',
+                    attachments: [csvFile]
+                });
+                log.audit('Auto FTP', 'Emails Successfully Sent');
             }
+        }
+        catch (error) {
+            log.error('Auto FTP', `There was an error with the Auto FTP --- ${error}`);
         }
     };
     exports.execute = execute;
--- FileCabinet/SuiteScripts/FPE_Scripts/Auto_InvFeed_Ftp/FPE_Sch_AutoInvFeed_FTP.ts
+++ FileCabinet/SuiteScripts/FPE_Scripts/Auto_InvFeed_Ftp/FPE_Sch_AutoInvFeed_FTP.ts
@@ -16,6 +16,7 @@ import * as file from 'N/file'
 import * as search from 'N/search'
 import * as runtime from 'N/runtime'
 import * as format from 'N/format'
+import * as email from 'N/email'
 
 interface QueryResult {
     itemid: string,
@@ -33,30 +34,36 @@ interface ConfigData {
 }
 
 export const execute: EntryPoints.Scheduled.execute = (scriptContext: EntryPoints.Scheduled.executeContext) => {
-    const sftpConfigIDs: string[] = (runtime.getCurrentScript().getParameter({ name: 'custscript_fpe_sftpconfid_ids'}) as string).split(',')
-    const csvFolder = runtime.getCurrentScript().getParameter({ name: 'custscript_fpe_csv_folder'}) as number
+    const currentScript = runtime.getCurrentScript()
 
-    if (sftpConfigIDs.length > 0 && csvFolder) {
-        try {
-            const fileTimestamp = format.format({value: new Date(), type: format.Type.DATETIME}).replaceAll('/', '').replaceAll(' ', '_').replaceAll(':', '')
+    try {
 
-            let csv = 'sku,qty\n'
+        const sftpConfigIDs: string[] = (currentScript.getParameter({ name: 'custscript_fpe_sftpconfid_ids' }) as string).split(',')
+        const customerIDs: string[] = (currentScript.getParameter({ name: 'custscript_fpe_emailcustomers_csv' }) as string).split(',')
+        const emailAuthorID: number = parseInt(currentScript.getParameter({ name: 'custscript_fpe_emailftp_author' }) as string)
+        const csvFolder = currentScript.getParameter({ name: 'custscript_fpe_csv_folder' }) as number
 
-            const data: QueryResult[] = query.runSuiteQL({query: queryString}).asMappedResults<QueryResult>()
-            log.audit('Auto FTP', 'Stock query completed')
+        const fileTimestamp = format.format({value: new Date(), type: format.Type.DATETIME}).replaceAll('/', '').replaceAll(' ', '_').replaceAll(':', '')
 
-            data.forEach(row => csv += `${row.itemid},${row.quantity}\n`)
-            log.audit('Auto FTP', 'CSV data generated')
+        let csv = 'sku,qty\n'
 
-            const csvFile = file.create({
-                fileType: file.Type.CSV,
-                name: `FpeStockFeed_${fileTimestamp}.csv`,
-                folder: csvFolder,
-                contents: csv
-            })
+        const data: QueryResult[] = query.runSuiteQL({query: queryString}).asMappedResults<QueryResult>()
+        log.audit('Auto FTP', 'Stock query completed')
 
-            const csvFileID = csvFile.save()
-            log.audit('Auto FTP', 'CSV file created and saved')
+        data.forEach(row => csv += `${row.itemid},${row.quantity}\n`)
+        log.audit('Auto FTP', 'CSV data generated')
+
+        const csvFile = file.create({
+            fileType: file.Type.CSV,
+            name: `FpeStockFeed_${fileTimestamp}.csv`,
+            folder: csvFolder,
+            contents: csv
+        })
+
+        const csvFileID = csvFile.save()
+        log.audit('Auto FTP', `CSV file created and saved: ${csvFileID}`)
+
+        if (sftpConfigIDs.length > 0 && csvFolder) {
 
             sftpConfigIDs.forEach(config => {
                 let configData: ConfigData = search.lookupFields({
@@ -66,26 +73,35 @@ export const execute: EntryPoints.Scheduled.execute = (scriptContext: EntryPoint
                 }) as ConfigData
                 log.audit('Auto FTP', 'sFTP Connection started')
 
-                try {
-                    let transfer = sftp.createConnection({
-                        url: configData.custrecord_fpe_sftp_host,
-                        port: parseInt(configData.custrecord_fpe_sftp_hostport),
-                        username: configData.custrecord_fpe_sftp_user,
-                        passwordGuid: configData.custrecord_fpe_sftp_passguid,
-                        hostKey: configData.custrecord_fpe_sftp_hostkey,
-                        directory: configData.custrecord_fpe_sftp_directory
-                    })
-                    log.audit('Auto FTP', 'sFTP Connected')
+                let transfer = sftp.createConnection({
+                    url: configData.custrecord_fpe_sftp_host,
+                    port: parseInt(configData.custrecord_fpe_sftp_hostport),
+                    username: configData.custrecord_fpe_sftp_user,
+                    passwordGuid: configData.custrecord_fpe_sftp_passguid,
+                    hostKey: configData.custrecord_fpe_sftp_hostkey,
+                    directory: configData.custrecord_fpe_sftp_directory
+                })
+                log.audit('Auto FTP', 'sFTP Connected')
 
-                    transfer.upload({ file: csvFile })
-                    log.audit('Auto FTP', 'File Succesfully transferred')
-                } catch (connectError) {
-                    log.error('Auto FTP Connection', `There was an error with the FTP connection --- ${connectError}`)
-                }
+                transfer.upload({ file: csvFile })
+                log.audit('Auto FTP', 'File Successfully transferred')
             })
-        } catch (error) {
-            log.error('Auto FTP', `There was an error with the Auto FTP --- ${error}`)
         }
+
+        if (customerIDs.length > 0 && csvFolder && emailAuthorID) {
+            log.audit('Auto FTP', 'Email Customers Found, Starting Email Process')
+
+            email.send({
+                author: emailAuthorID,
+                recipients: customerIDs,
+                subject: 'Fleece Performance Engineering Daily Stock Feed',
+                body: 'This daily stock feed is representative of our inventory available for drop-shipments only at the time of publishing and does not apply to pending or new stocking orders. Dropship orders for in-stock items received before 2:00 PM EST will be shipped on the same day.',
+                attachments: [ csvFile ]
+            })
+            log.audit('Auto FTP', 'Emails Successfully Sent')
+        }
+    } catch (error) {
+        log.error('Auto FTP', `There was an error with the Auto FTP --- ${error}`)
     }
 }
 
